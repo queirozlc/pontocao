9c2f07c17595ea670ee340a9cc08926916b653c4
lucasqueiroz <sepulchrolucas@gmail.com>
2024-04-16 23:01:30 -0300

    Initial commit
    Signed-off-by: lucasqueiroz <sepulchrolucas@gmail.com>

---
+++ mobile/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View className="flex-1 items-center justify-center bg-red-500">
+      <Text className="text-2xl text-zinc-950">Hello World</Text>
+    </View>
+  );
+}
