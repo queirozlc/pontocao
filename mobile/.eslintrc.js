2a2aebde92b09ad6d79b4602d2b9ecd4ef30f82e
lucasqueiroz <sepulchrolucas@gmail.com>
2024-04-25 15:11:39 -0300

    update eslint rules

--- mobile/.eslintrc.js
+++ mobile/.eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   extends: ['universe/native', 'plugin:tailwindcss/recommended'],
   rules: {
     // Ensures props and state inside functions are always up-to-date
-    'react-hooks/exhaustive-deps': 'warn'
+    'react-hooks/exhaustive-deps': 'warn',
+    'no-undef': 'off'
   }
 }
