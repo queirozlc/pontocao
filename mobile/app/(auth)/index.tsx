97155a9c81665077edc8a039d8198c0b70e8554e
lucasqueiroz <sepulchrolucas@gmail.com>
2024-04-25 15:11:50 -0300

    initial route structure

---
+++ mobile/app/(auth)/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native'
+
+export default function SignUpScreen() {
+  return (
+    <View className="flex-1 items-center justify-center bg-emerald-500">
+      <Text className="font-title-bold text-2xl text-white">Hello World</Text>
+    </View>
+  )
+}
