fc2d10f2389e29fa342cb5241f8cfd257949a572
lucasqueiroz <sepulchrolucas@gmail.com>
2024-04-25 15:11:28 -0300

    add font setup

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,5 +1,11 @@
 import '@/styles/global.css'
 import {
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_700Bold
+} from '@expo-google-fonts/inter'
+import {
   DarkTheme,
   DefaultTheme,
   ThemeProvider
@@ -19,7 +25,14 @@ export {
 SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
-  const [loaded, error] = useFonts({})
+  const [loaded, error] = useFonts({
+    Cabinet_Grotestk_500Medium: require('@/assets/fonts/CabinetGrotesk-Medium.otf'),
+    Cabinet_Grotestk_700Bold: require('@/assets/fonts/CabinetGrotesk-Bold.otf'),
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Inter_700Bold
+  })
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
   useEffect(() => {
--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -3,7 +3,16 @@ module.exports = {
   content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        'sans-regular': ['Inter_400Regular'],
+        'sans-medium': ['Inter_500Medium'],
+        'sans-semibold': ['Inter_600SemiBold'],
+        'sans-bold': ['Inter_700Bold'],
+        'title-medium': ['Cabinet_Grotestk_500Medium'],
+        'title-bold': ['Cabinet_Grotestk_700Bold']
+      }
+    }
   },
   plugins: []
 }
