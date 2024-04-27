9bbe6b5ef24d233218dab678b41ba8140646d594
lucasqueiroz <sepulchrolucas@gmail.com>
2024-04-27 10:39:29 -0300

    add a `Container` component based on `ScrollView` tag

--- mobile/components/ui/base.tsx
+++ mobile/components/ui/base.tsx
@@ -1,10 +1,11 @@
 import { ComponentPropsWithRef, forwardRef } from 'react'
-import { View } from 'react-native'
+import { ScrollView, View } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 import { cn } from '@/lib/utils'
 
 type ViewProps = ComponentPropsWithRef<typeof View>
+type ScrollViewProps = ComponentPropsWithRef<typeof ScrollView>
 
 export const Container = forwardRef<View, ViewProps>((props, ref) => {
   const { top } = useSafeAreaInsets()
@@ -20,6 +21,27 @@ export const Container = forwardRef<View, ViewProps>((props, ref) => {
     </View>
   )
 })
+
+export const ContainerScroll = forwardRef<ScrollView, ScrollViewProps>(
+  ({ children, className, ...props }, ref) => {
+    const { top } = useSafeAreaInsets()
+
+    return (
+      <ScrollView
+        ref={ref}
+        {...props}
+        className={cn('flex-1 bg-white px-6 dark:bg-zinc-800', className)}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[
+          { paddingTop: top },
+          props.contentContainerStyle
+        ]}
+      >
+        {children}
+      </ScrollView>
+    )
+  }
+)
 
 export const Row = forwardRef<View, ViewProps>((props, ref) => {
   return (
