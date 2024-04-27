315e91c58aec5ac1a2438c7a259812a7a20bb0a0
lucasqueiroz <sepulchrolucas@gmail.com>
2024-04-27 11:18:57 -0300

    add search screen initial layout

--- mobile/app/(app)/home/search.tsx
+++ mobile/app/(app)/home/search.tsx
@@ -1,5 +1,65 @@
-import { Container } from '@/components/ui/base'
+import { Feather } from '@expo/vector-icons'
+import { Text, View } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import colors from 'tailwindcss/colors'
+
+import { Badge, BadgeText } from '@/components/ui/badge'
+import { Container, Row } from '@/components/ui/base'
+import { Input } from '@/components/ui/input'
 
 export default function Search() {
-  return <Container />
+  const { top } = useSafeAreaInsets()
+
+  return (
+    <Container style={{ paddingTop: top + 60 }} className="gap-6">
+      <Row className="items-center">
+        <Feather
+          name="search"
+          size={24}
+          color={colors.zinc['400']}
+          className="absolute left-4"
+        />
+        <Input
+          variant="outlined"
+          className="rounded-full pl-14"
+          style={{
+            elevation: 2,
+            shadowColor: colors.zinc['400'],
+            shadowOffset: {
+              width: 0,
+              height: 2
+            },
+            shadowOpacity: 0.25,
+            shadowRadius: 3.84
+          }}
+        />
+      </Row>
+
+      <View className="gap-4">
+        <Row className="items-center justify-between">
+          <Text className="font-sans-semibold text-lg text-zinc-800 dark:text-zinc-200">
+            Recentes
+          </Text>
+
+          <Text className="font-sans-semibold text-lg text-brand-500">
+            Limpar
+          </Text>
+        </Row>
+
+        <Row className="items-center gap-4">
+          <Badge>
+            <BadgeText>Cachorro</BadgeText>
+          </Badge>
+
+          <Badge>
+            <BadgeText>UVV</BadgeText>
+          </Badge>
+
+          <Badge>
+            <BadgeText>ONG Amor de Bicho</BadgeText>
+          </Badge>
+        </Row>
+      </View>
+    </Container>
+  )
 }
