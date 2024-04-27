f19a33215aa4fb454a9b880f719f98f3bc1f473d
lucasqueiroz <sepulchrolucas@gmail.com>
2024-04-27 10:38:54 -0300

    add constants for device dimensions

--- mobile/lib/constants.ts
+++ mobile/lib/constants.ts
@@ -1,7 +1,11 @@
+import { Dimensions } from 'react-native'
+
 export const Constants = {
   BRAND_COLOR: '#FF407D',
   FONT_SANS: {
     400: 'Inter_400Regular',
     500: 'Inter_500Medium'
-  }
+  },
+  DEVICE_WIDTH: Dimensions.get('window').width,
+  DEVICE_HEIGHT: Dimensions.get('window').height
 } as const
