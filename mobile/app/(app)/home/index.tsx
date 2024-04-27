9b8d3b409268a307fa8badf247f930cfb51b4fd9
lucasqueiroz <sepulchrolucas@gmail.com>
2024-04-27 10:48:27 -0300

    initial home layout

--- mobile/app/(app)/home/index.tsx
+++ mobile/app/(app)/home/index.tsx
@@ -1,13 +1,34 @@
-import { FeedHeader } from '@/components/feed-header'
-import { PetCategories } from '@/components/pet-categories'
-import { Container } from '@/components/ui/base'
+import { View } from 'react-native'
+
+import PetBanner from '@/assets/images/feed-banner.svg'
+import { FeedHeader } from '@/components/home/feed-header'
+import { PetCategories } from '@/components/home/pet-categories'
+import { PetsNearYou } from '@/components/home/pets-near-you'
+import { ContainerScroll } from '@/components/ui/base'
+import { Constants } from '@/lib/constants'
 
 export default function HomeScreen() {
   return (
-    <Container className="gap-6">
-      <FeedHeader />
+    <ContainerScroll contentContainerClassName="gap-y-9">
+      <View className="gap-4">
+        <FeedHeader />
+        <View className="items-center justify-center">
+          <PetBanner
+            width={Constants.DEVICE_WIDTH - 32}
+            height={Constants.DEVICE_HEIGHT * 0.2}
+          />
+        </View>
+        <PetCategories />
+      </View>
 
-      <PetCategories />
-    </Container>
+      {/*
+        The available sections are:
+        - PetsNearYou (Pets próximos de você)
+        - InstitutionsEvents (Eventos de instituições)
+        - InstitutionsNearYou (Instituições próximas de você)
+        - PetsForAdoption (Pets para adoção)
+      */}
+      <PetsNearYou />
+    </ContainerScroll>
   )
 }
