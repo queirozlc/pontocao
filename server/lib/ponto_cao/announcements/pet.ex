e564fb5f028c4b9bfed05ab6a22b4f98244ec18f
lucasqueiroz <sepulchrolucas@gmail.com>
2024-05-20 19:25:32 -0300

    `server`: update pets schema in order to add breeds and change validations
    I decided to only support cat and dogs announcements at this moment,
    that's why the field species changed

--- server/lib/ponto_cao/announcements/pet.ex
+++ server/lib/ponto_cao/announcements/pet.ex
@@ -1,6 +1,7 @@
 defmodule PontoCao.Announcements.Pet do
   use Ecto.Schema
   import Ecto.Changeset
+  alias PontoCao.Announcements
   alias PontoCao.{Accounts, Commons}
 
   schema "pets" do
@@ -10,8 +11,7 @@ defmodule PontoCao.Announcements.Pet do
     field :photos, {:array, :string}
     field :age, :integer
     field :gender, Ecto.Enum, values: [:MALE, :FEMALE]
-    field :breed, :string
-    field :species, :string
+    field :species, Ecto.Enum, values: [:DOG, :CAT]
     field :vaccinated, :boolean, default: false
     field :dewormed, :boolean, default: false
     field :neutered, :boolean, default: false
@@ -19,6 +19,7 @@ defmodule PontoCao.Announcements.Pet do
     field :pedigree, :boolean, default: false
     field :weight, :decimal
     belongs_to :user, Accounts.User, foreign_key: :owner_id
+    belongs_to :breed, Announcements.Breed, foreign_key: :breed_id
 
     timestamps(type: :utc_datetime)
   end
@@ -32,7 +33,6 @@ defmodule PontoCao.Announcements.Pet do
       :photos,
       :age,
       :gender,
-      :breed,
       :species,
       :vaccinated,
       :dewormed,
@@ -41,7 +41,8 @@ defmodule PontoCao.Announcements.Pet do
       :pedigree,
       :size,
       :weight,
-      :owner_id
+      :owner_id,
+      :breed_id
     ])
     |> validate_required([
       :name,
@@ -57,12 +58,15 @@ defmodule PontoCao.Announcements.Pet do
       :pedigree,
       :size,
       :weight,
-      :owner_id
+      :owner_id,
+      :breed_id
     ])
     |> foreign_key_constraint(:owner_id)
+    |> foreign_key_constraint(:breed_id)
     |> validate_number(:age, greater_than: 0)
     |> validate_inclusion(:gender, [:MALE, :FEMALE])
-    |> validate_length(:photos, min: 1)
+    |> validate_inclusion(:species, [:DOG, :CAT])
+    |> validate_length(:photos, min: 2, max: 5)
     |> validate_number(:size, greater_than: 0)
     |> validate_number(:weight, greater_than: 0)
     |> Commons.Validations.validate_urls(:photos)
