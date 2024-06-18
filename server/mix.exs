65e1a7e428d0d453f258ee02cdf9da6a381da25f
caslu <sepulchrolucas@gmail.com>
2024-06-18 18:06:27 -0300

    add `bcrypt` library to hash passwords

--- server/mix.exs
+++ server/mix.exs
@@ -34,6 +34,7 @@ defmodule PontoCao.MixProject do
     [
       {:phoenix, "~> 1.7.11"},
       {:phoenix_ecto, "~> 4.4"},
+      {:bcrypt_elixir, "~> 3.0"},
       {:ecto_sql, "~> 3.10"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_live_dashboard, "~> 0.8.3"},
