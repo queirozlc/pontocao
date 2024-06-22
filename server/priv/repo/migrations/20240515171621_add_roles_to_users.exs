301c2e17827c7239b5e1286805de9c19a619e2c4
caslu <sepulchrolucas@gmail.com>
2024-06-22 01:35:14 -0300

    refactor: Remove migration for adding roles to users

--- server/priv/repo/migrations/20240515171621_add_roles_to_users.exs
+++
@@ -1,9 +0,0 @@
-defmodule PontoCao.Repo.Migrations.AddRolesToUsers do
-  use Ecto.Migration
-
-  def change do
-    alter table(:users) do
-      add :roles, {:array, :string}, null: false
-    end
-  end
-end
