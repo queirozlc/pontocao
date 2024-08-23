cb603bc7c210e900f788e62fb0a7a8e3c16c832e
caslu <sepulchrolucas@gmail.com>
2024-08-23 00:23:37 -0300

    remove unused extensions

--- server/priv/repo/migrations/20240510000227_create_extensions.exs
+++ server/priv/repo/migrations/20240510000227_create_extensions.exs
@@ -2,8 +2,6 @@ defmodule PontoCao.Repo.Migrations.CreateExtensions do
   use Ecto.Migration
 
   def change do
-    execute "CREATE EXTENSION IF NOT EXISTS pg_trgm;", "DROP EXTENSION IF EXISTS pg_trgm;"
-    execute "CREATE EXTENSION IF NOT EXISTS postgis;", "DROP EXTENSION IF EXISTS postgis;"
-    execute "CREATE EXTENSION IF NOT EXISTS unaccent", "DROP EXTENSION IF EXISTS unaccent;"
+    execute "CREATE EXTENSION IF NOT EXISTS postgis;"
   end
 end
